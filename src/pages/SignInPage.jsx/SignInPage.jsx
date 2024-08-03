65eaaadb28acd30f0266d31e5b6883c8292e81ee
Le Manh Hung <hungoffsize@gmail.com>
2024-08-03 20:56:02 +0700

    [Add] SignInPage

---
+++ src/pages/SignInPage.jsx/SignInPage.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { WrapperContainerLeft, WrapperContainerRight } from "./style";
+
+export const SignInPage = () => {
+  return (
+    <div>
+      <WrapperContainerLeft>
+        <h1>Hello</h1>
+        <p>Login or Register</p>
+      </WrapperContainerLeft>
+      <WrapperContainerRight></WrapperContainerRight>
+    </div>
+  );
+};
+export default SignInPage;
